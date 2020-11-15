0f115df157a0f7def0503d3dcb3d0a5532bee318
Jordan <jordanleelong5@gmail.com>
2020-11-14 23:37:46 -0800

    created imports and passed tests for App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react'; 
-// import {BrowserRouter, Route} from 'react-router-dom';
-
+import {BrowserRouter, Route} from 'react-router-dom';
+import Feed from './components/Feed'; 
+import Search from './components/Search'; 
+import Notifications from './components/Notifications'; 
+import Messages from './components/Messages';
 
 class App extends React.Component {
   render() {
     return (
       <div data-test="links-container">
-        This is where we'll define our links that our side nav will need
+        <BrowserRouter data-test="browser-router-tag">
+      <Route data-test='routes' exact path='/' component={Feed} />
+      <Route data-test='routes' exact path='/search' component={Search}/>
+      <Route data-test='routes' exact path='/notifications' component={Notifications}/>
+      <Route data-test='routes' exact path='/messages' component={Messages} />
+        </BrowserRouter>
       </div>
     )
   }
