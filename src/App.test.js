cbac8bf85410c381c23a761fa6e4914c2fc69a0c
Jordan <jordanleelong5@gmail.com>
2020-11-14 23:07:36 -0800

    imported enzyme, wrote one test to check if working. it is

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import Enzyme, {shallow} from 'enzyme'
+import EnzymeAdapter from 'enzyme-adapter-react-16'
+import {findByTestAttr} from './test/testHelpers.test'
+Enzyme.configure({adapter: new EnzymeAdapter()}); 
+
+const setUp = () => shallow(<App />); 
+
+test('renders links container without throwing an error', () => {
+  const wrapper = setUp(); 
+  const linksContainer = findByTestAttr(wrapper, 'links-container'); 
+  expect(linksContainer.length).toBe(1);
 });
