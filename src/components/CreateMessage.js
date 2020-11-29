3a17c489e9d59550c94d93f808da88d991ccb37e
Jordan <jordanleelong5@gmail.com>
2020-11-28 22:58:25 -0800

    boiler plate for create message comp

---
+++ src/components/CreateMessage.js
@@ -0,0 +1,13 @@
+import React from 'react'; 
+
+class CreateMessage extends React.Component {
+    render() {
+        return (
+            <div>
+                Create a message 
+            </div>
+        );
+    };
+};
+
+export default CreateMessage;
